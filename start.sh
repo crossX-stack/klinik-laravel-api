7372345f5456190735ac6b662a770440c91ebb4f
crossX-stack <victorkarani35@gmail.com>
2025-04-01 15:58:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# Run Migrations
+# Clear and refresh Laravel caches
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+php artisan route:clear
+php artisan view:clear
+
+# Run database migrations
 php artisan migrate --force
 
-# Start Laravel Server
+# Start Laravel server
 php artisan serve --host 0.0.0.0 --port 10000
